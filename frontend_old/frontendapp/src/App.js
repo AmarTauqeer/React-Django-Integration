256804648c41f762103c88e7507e9179af5d2a10
Amar Tauqeer <amar.tauqeer@gmail.com>
2020-10-15 13:29:35 +0200

    frontend folder changes

---
+++ frontend_old/frontendapp/src/App.js
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+import "./App.css";
+import Login from "./components/Login";
+import Books from "./components/Book";
+
+function App() {
+  const [token, setToken] = useState("");
+
+  const userLogin = (tok) => {
+    setToken(tok);
+  };
+
+  return (
+    <div className="App">
+      <Login userLogin={userLogin} />
+      <Books token={token} />
+    </div>
+  );
+}
+
+export default App;
